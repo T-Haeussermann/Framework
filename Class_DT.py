4a7a46b679bdfaa5a333812c3196268d88daa45a
Tim Häußermann <timi.haeussermann@googlemail.com>
2022-04-10 11:42:25 +0200

    Upload

--- Class_DT.py
+++ Class_DT.py
@@ -1,6 +1,4 @@
-#class Digital Twin(Asset Digital Twin,Product Demand Digital Twin):
 import time
-
 
 class Digital_Twin:
     """Klasse Digital Twin, Quelle: https://medium.com/swlh/classes-subclasses-in-python-12b6013d9f3"""
@@ -14,34 +12,14 @@ class Digital_Twin:
             time.sleep(5)
 
 
-# class Asset_Digital_Twin(Digital_Twin):
-#     def __init__(self, name, namePT, typ, skill, processing_time, capacity):
-#         #self.typ = "Asset Digital Twin"
-#         self.skill = skill
-#         self.processing_time = processing_time
-#         self.capacity = capacity
-#
-#     def use_skill(self):
-#         print("Skill eingesetzt")
-#
-#     def accept_demand(self):
-#         print("Anfrage akzeptiert")
-#
-# class Product_Demand_Digital_Twin(Digital_Twin):
-#     def __init__(self, name, namePT, typ, demand, date_of_delivery):
-#         #self.typ = "Asset Digital Twin"
-#         self.demand = demand
-#         self.date_of_delivery = date_of_delivery
-#
-#     def submit_demand(self):
-#         print("Anfragegestellt")
+class Asset_Digital_Twin(Digital_Twin):
+    def __init__(self, Name, Typ, Fähigkeit):
+        super().__init__(Name, Typ, Fähigkeit)
+        self.Fähigkeit = Fähigkeit
 
 
-#
-#
 
-#
-#
-# Digital_Twin.on_msg(DT)
-# Digital_Twin.send_msg(DT)
-# Digital_Twin.evaluete_data(DT)
+class Product_Demand_Digital_Twin(Digital_Twin):
+    def __init__(self, Name, Typ, Bedarf):
+        super().__init__(Name, Typ, Bedarf)
+        self.Bedarf = Bedarf
--- Laufumgebung.py
+++ Laufumgebung.py
@@ -1,6 +1,8 @@
 import paho.mqtt.client as mqtt
 import json
 import threading
+import multiprocessing
+import asyncio
 from Class_DT import Digital_Twin
 
 
@@ -20,24 +22,33 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     TopicUndNachricht = msg.topic + " : " + str(msg.payload.decode("utf-8")) #string
     Nachricht = json.loads(str(msg.payload.decode("utf-8")))
-    if ("/Anforderung" in TopicUndNachricht and Nachricht["Task"] == "Erstelle DT"):
+
+    if ("/Anforderung" in TopicUndNachricht and Nachricht["Task"] == "Erstelle DT" and Nachricht["Name"] not in threading.enumerate()):
         print("Ich stelle DT mit dem Namen " + Nachricht["Name"] + " bereit!")
         Neuer_DT = Digital_Twin(Nachricht["Name"], Nachricht["Typ"])
-        print(Neuer_DT.Name + " Aus der Laufumgebung gesendet")
-        DT_Thread = threading.Thread(target=Digital_Twin.DT_Ablauf(Neuer_DT))
+        print(Neuer_DT.Name + " vom Typ " + Neuer_DT.Typ + " Aus der Laufumgebung gesendet")
+        DT_Thread = threading.Thread(name=Neuer_DT.Name, target=Neuer_DT.DT_Ablauf)
         DT_Thread.start()
-        #DT_Thread.join()
+        print(DT_Thread.name + " name of thread")
+
+    elif (Nachricht["Name"] in threading.enumerate()):
+        print("Ich lege keinen neuen DT an")
 
 
-client = mqtt.Client()
-client.username_pw_set(_username, _passwd)
-client.on_connect = on_connect
-client.on_message = on_message
-client.connect(_host, _port, _timeout)
 
-# Payload=json.dumps({"Maschine-17": "Erstelle DT"})
-# topic = "Laufumgebung"
-# client.publish(topic, Payload)
-print("ich bin die Laufumgebung")
+def Laufumgebung():
+    client = mqtt.Client()
+    client.username_pw_set(_username, _passwd)
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect(_host, _port, _timeout)
 
-client.loop_forever()
+    # Payload=json.dumps({"Maschine-17": "Erstelle DT"})
+    # topic = "Laufumgebung"
+    # client.publish(topic, Payload)
+    print("ich bin die Laufumgebung")
+
+    client.loop_forever()
+
+if __name__ == '__main__':
+    Laufumgebung()
